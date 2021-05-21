8ce79c7397bcdf90bbb1629981ba93455d52b287
Mohit Kumar <mohitk@geekyants.com>
2021-05-21 21:03:24 +0530

    feat(token_validation) : Added middleware to manage validations

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -9,7 +9,6 @@ const {
   deleteUser,
   getUserByEmail,
 } = require("./user.service");
-
 const { genSaltSync, hashSync, compareSync } = require("bcrypt");
 const { sign } = require("jsonwebtoken");
 
@@ -21,6 +20,7 @@ module.exports = {
    */
   createUser: (req, res) => {
     const body = req.body;
+    const loginBody = { email: body.email, password: body.password };
     const salt = genSaltSync(10);
     body.password = hashSync(body.password, salt);
     create(body, (err, results) => {
--- api/users/user.router.js
+++ api/users/user.router.js
@@ -11,11 +11,13 @@ const {
 } = require("./user.controller");
 const router = require("express").Router();
 
-router.post("/create-user", createUser);
-router.get("/get-users", getUsers);
-router.get("/get-user-by-id/:id", getUserById); // => when data is defined inside root as params [url-params]
-router.delete("/delete-user", deleteUser);
-router.patch("/update-user", updateUser);
+const { checkToken } = require("../../auth/token_validation");
+
+router.post("/create-user", checkToken, createUser);
+router.get("/get-users", checkToken, getUsers);
+router.get("/get-user-by-id/:id", checkToken, getUserById); // => when data is defined inside root as params [url-params]
+router.delete("/delete-user", checkToken, deleteUser);
+router.patch("/update-user", checkToken, updateUser);
 router.post("/login", login);
 
 module.exports = router;
